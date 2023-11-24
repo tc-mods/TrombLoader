e85bedddfb9b63d6c7d124ce62f479d4c7f5a173
DerpyChap <holla@derpychap.co.uk>
2023-11-24 01:05:42 +0000

    add config options for default background

--- CustomTracks/Backgrounds/EmptyBackground.cs
+++ CustomTracks/Backgrounds/EmptyBackground.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 namespace TrombLoader.CustomTracks.Backgrounds;
 
 /// <summary>
-///  Background used when a custom track doesn't specify one. Ends up being a grey slab.
+///  Background used when a custom track doesn't specify one. Configured via TrombLoader.cfg.
 /// </summary>
 public class EmptyBackground : HijackedBackground
 {
     public override void SetUpBackground(BGController controller, GameObject bg)
     {
-        // TODO: check TrombLoader config for background preference
-        // DisableParts(bg);
-        
-        // this is just to enable the background animations
-        controller.songname = "freeplay";
+        switch(Plugin.Instance.DefaultBackground.Value)
+        {
+            case "grey":
+                DisableParts(bg);
+                break;
+            case "black":
+                var camera = bg.GetComponent<Camera>();
+                camera.backgroundColor = Color.black;
+                DisableParts(bg);
+                break;
+            case "freeplay-static":
+                break;
+            default:
+                // this is just to enable the background animations
+                controller.songname = "freeplay";
+                break;
+        }
     }
 
     public override bool CanResume => true;
--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using BaboonAPI.Hooks.Initializer;
 using BaboonAPI.Hooks.Tracks;
@@ -24,6 +25,7 @@ namespace TrombLoader
 
         public ConfigEntry<int> beatsToShow;
         public ConfigEntry<bool> DeveloperMode;
+        public ConfigEntry<string> DefaultBackground;
 
         private Harmony _harmony = new(PluginInfo.PLUGIN_GUID);
 
@@ -33,6 +35,15 @@ namespace TrombLoader
             beatsToShow = customFile.Bind("General", "Note Display Limit", 64, "The maximum amount of notes displayed on screen at once.");
             DeveloperMode = customFile.Bind("Charting", "Developer Mode", false,
                 "When enabled, TrombLoader will re-read chart data from disk each time a track is loaded.");
+            
+            var backgrounds = new List<string>{"freeplay", "freeplay-static", "grey", "black"};
+            DefaultBackground = customFile.Bind("General", "Default Background", "freeplay", 
+                $"The default background to show when a chart does not include one. Can be one of the following:\n{string.Join(", ", backgrounds)}");
+            DefaultBackground.Value = DefaultBackground.Value.ToLower().Trim();
+            if (!backgrounds.Contains(DefaultBackground.Value))
+            {
+                LogWarning("Default Background is not set to a valid option!");
+            }
 
             Instance = this;
             LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
