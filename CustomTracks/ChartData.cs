2902b2185fa9d30f8d26395ce1c2dd630064c485
Emmett Lin <emmett@linbarbell.com>
2024-10-01 01:22:03 -0400

    don't log all of my songs!!!!!!!

--- CustomTracks/ChartData.cs
+++ CustomTracks/ChartData.cs
@@ -14,7 +14,7 @@ public class ChartData
     public int timesig;
 
     [JsonRequired] public float[][] notes;
-    public List<Lyric> lyrics = [];
+    public List<Lyric> lyrics = new();
     public float[] note_color_start = { 1.0f, 0.21f, 0f };
     public float[] note_color_end = { 1.0f, 0.8f, 0.3f };
     public float[][] improv_zones = {};
